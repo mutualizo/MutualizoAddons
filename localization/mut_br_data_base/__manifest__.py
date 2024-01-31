4261e121b1fe7d6777ec1eacb32d217c668115d2
defendi <alexandre.defendi@mutualizo.com>
2024-01-31 11:23:58 -0300

    Add: module data brasil localization base

---
+++ localization/mut_br_data_base/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Brazilian Localisation Data Extension for Base',
+    'description': 'Brazilian Localisation Data Extension for Base',
+    'license': 'AGPL-3',
+    'author': '',
+    'version': '17.0.0.1',
+    'depends': [
+        'mut_br_base',
+    ],
+    'data': [
+        'data/res.bank.csv',
+        'data/res.country.csv',
+        'data/res.country.state.csv',
+        'data/br_base_data.xml',
+    ],
+    'demo': [],
+    'category': 'Localisation',
+    'installable': True,
+    'auto_install': False,
+}
