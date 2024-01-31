d255f413779929d5d0612ce50ef0fd9b29d2f53e
defendi <alexandre.defendi@mutualizo.com>
2024-01-31 20:26:56 -0300

    Configuração de validação do modulo base

--- localization/mut_br_base/__manifest__.py
+++ localization/mut_br_base/__manifest__.py
@@ -28,6 +28,7 @@
         'views/res_country_state_view.xml',
         'views/res_partner_view.xml',
         'views/res_company_view.xml',
+        'views/res_config_settings_view.xml',
         'views/menus_view.xml',
     ],
     'external_dependencies': {},
