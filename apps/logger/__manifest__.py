a00329552e63e0f351e4ab6164e65651c52b7fee
Daniel Paloschi <danielpaloschi.dev@gmail.com>
2024-05-03 14:26:17 -0300

    Migrate auth_aws_cognito to v.14

--- apps/logger/__manifest__.py
+++ apps/logger/__manifest__.py
@@ -1,33 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "log",
-
+    'version': "14.0.0.0",
+    'category': 'Extra Tools',
     'summary': "Saves logs to a table",
-
-    'description': """
-Saves logs to a table
-    """,
-
-    'author': "Joao Amorim",
-    'website': "https://www.yourcompany.com",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'description': "Saves logs to a table",
+    'author': "Joao Amorim, Mutualizo",
+    'company': 'Mutualizo',
+    'maintainer': 'Mutualizo',
+    'website': "https://www.mutualizo.com.br",
     'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
