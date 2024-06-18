3939f27084af376f578bbf63cfcb0cc897fadc19
Daniel Paloschi <danielpaloschi.dev@gmail.com>
2024-06-18 10:03:18 -0300

    Create chart of accounts module

---
+++ apps/mut_coa/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Chart of Accounts Mutualizo",
+    "version": "14.0.0.0",
+    "category": "Extra Tools",
+    "description": """Plano de Contas Mutualizo""",
+    "author": "Mutualizo",
+    "company": "Mutualizo",
+    "maintainer": "Mutualizo",
+    "website": "https://www.mutualizo.com.br",
+    "depends": [
+        "l10n_br_coa",
+    ],
+    "data": [
+        # Data
+        "data/coa_template.xml",
+        "data/account_account_type.xml",
+        "data/account_group_template.xml",
+        "data/account.account.template.csv",
+    ],
+}
