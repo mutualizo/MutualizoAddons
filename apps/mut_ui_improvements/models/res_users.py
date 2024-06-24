10f6aa3a827c292d10c289f0081bf5a7a6da8123
Felipe Paloschi <165680980+paloschi-mutualizo@users.noreply.github.com>
2024-06-24 16:53:04 -0300

    Update callback and boleto description (#120)
    * Fix app overlay action
    
    * Update callbacks keys. Make contract fields readonly on account_move
    
    * Add 'additional_description_installment' to boleto PDF
    
    ---------
    
    Co-authored-by: Felipe Paloschi <paloschi.eca@gmail.com>
    Co-authored-by: Daniel Paloschi <danielpaloschi.dev@gmail.com>

--- apps/mut_ui_improvements/models/res_users.py
+++ apps/mut_ui_improvements/models/res_users.py
@@ -8,7 +8,7 @@ class ResUsers(models.Model):
         res = super(ResUsers, self).read(fields=fields, load=load)
         # We only want to redirect to app lists if the read is only in the
         # user default action. Then, we redirect to the apps list page
-        if fields == ["action_id"] and not res[0].get("action_id"):
+        if "action_id" in fields and not res[0].get("action_id"):
             action = self.env.ref("mut_ui_improvements.action_redirect_to_apps").sudo()
             res[0]["action_id"] = (action.id, action.name)
         return res
